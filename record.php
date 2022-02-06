e5bb15ba8744c3741adc6ee8bd798a67a819eeda
SumedhaMahendra <92027998+SumedhaMahendra@users.noreply.github.com>
2022-02-06 19:52:56 +0530

    commit

--- record.php
+++ record.php
@@ -2,20 +2,36 @@
 <html>
     <head>
         <title></title>
-        <link rel="icon" href="imgs/favicon.jpg" type="image/ico">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Quintessential&display=swap" rel="stylesheet">
         <style>
-            body{background: linear-gradient(90deg, rgba(159,207,231,1) 0%, rgba(199,228,234,1) 63%, rgba(187,171,234,1) 100%);}
-            .col{padding: 50px;}
-            img{
-                min-width: 400px;
-                border-radius: 10px;
+            body
+            {
+                background-color: rgba(165, 240, 238,0.6)
             }
-            input{margin: 10px;}
-            .container{
+            input
+            {
+                margin: 10px;
+            }
+            .profileimg
+            {
+                border-radius: 50%;
+                height: 50px;
+                width: 50px;
+            }
+            .container
+            {
+                background-color: rgba(146, 225, 247,0.8);
                 border: 1px solid darkblue;
                 border-radius: 20px;
                 padding: 30px;
+                margin-top: 5%;
+                box-shadow: 5px 10px 18px black;
+            }
+            h1
+            {
+                font-family: 'Quintessential', cursive;
             }
         </style>
     </head>
@@ -23,19 +39,43 @@
         <?php
         include('DB_Connect.php');
         ?>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="home.php">Attendance Portal</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="manage.php">Manage</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="record.php">Record</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+                        <a class="nav-link active" aria-current="page" href="logout.php">Logout</a>
+                    </span>
+                </div>
+                <img src="imgs/a1.png" class="profileimg">
+            </div>
+    </nav>
      <div class="container">
-        <h2 class="alert alert-primary">Attendance Records</h2>
+        <h1>Attendance Records</h1>
         <div class="row">
             <div class="col">
                 <form action="" method="post">
                     <div class="form-group">
-                        <input type="month" name="month" required>
+                        <label class="form-check-label" for="month">Select Date to see record:</label>
+                        <input type="month" name="month" id="month" required>
                     </div>
                     <br>
                     <div class="form-group">
-                        <input class="form-control" type="number" name="roll_no" placeholder = "Enter Roll Number" required>
+                        <label class="form-check-label" for="rollno">Enter roll number of the student:</label>
+                        <input class="form-control" type="number" name="roll_no" id="rollno" placeholder = "Enter Roll Number" required>
                     </div>
-                    <br>
+                
                     <input class="btn btn-primary" type="submit" name="submit" value="Show Attendance Records">
                 </form>
             </div>
